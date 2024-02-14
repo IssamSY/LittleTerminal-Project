aac8aee26a282f0b546fed44e678c369a9c07b08
S. Issam <57065659+IssamSY@users.noreply.github.com>
2024-02-14 22:44:59 +0100

    Update terminal.c
    Even more issues and question arises as my ideas come to application. Headache.

--- terminal.c
+++ terminal.c
@@ -4,18 +4,22 @@
 
 int main() {
 	// Creating files using `struct File`
-	struct File systemf;
-	strcpy(systemf.name, "systemf.txt");
-	strcpy(systemf.content, "The System F version 1.2.2");
-
-	struct File user_info;
-	strcpy(user_info.name, "user_info.txt");
-	strcpy(user_info.content, "The username: _____");
+	struct File systemf_txt = createFile("systemf.txt", "The System F version 1.2.2");
+	struct File userInfo_txt = createFile("userInfo.txt", "The username: _____");
 
 	// Creating `sysbin` directory using `struct Directory`
 	struct Directory sysbin;
 	snprintf(sysbin.name, sizeof(sysbin.name), "sysbin");
 
+	struct Directory sysbin = createDirectory("sysbin", systemf_txt, 1);
+	/*
+ 	potenial issues on the line 14: 
+  	-	The second argument of the function prototype `createDirectory` may be confusing to deal with.
+   	-	Can I put the statemet in line 12 inside the header file's prototype function?
+ 	-	I want the program to treat `systemf.txt` as an array, and I suspect it wont. 
+ 	*/
+
+	
 	// Adding `systemf.txt` file to `sysbin` directory in position 0
 	strcpy(sysbin.files[0].name, systemf.name);
 	strcpy(sysbin.files[0].content, systemf.content);
