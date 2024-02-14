df3a7d352e5977e1c2ee8df4a81e7159619d656d
S. Issam <57065659+IssamSY@users.noreply.github.com>
2024-02-14 11:27:32 +0100

    Update FileSys.h
    Finally I figured out how to define a header file. :)

--- FileSys.h
+++ FileSys.h
@@ -1,2 +1,36 @@
-// A header file for File Management System
-// In progress..
+/*
+	Created by S. Issam
+	Copyright 452
+	You may use or redistribute this file, however you have to credit the creator.
+*/
+#ifndef SYSFILE_H
+#define SYSFILE_H
+
+// Defining some constants
+#define MAX_NAME_LENGHT 30
+#define MAX_CONTENT_LENGHT 100
+#define MAX_FILES_LENGHT 20
+#define MAX_DIRECTORIES_LENGHT 20
+
+struct File {
+	char name[MAX_NAME_LENGHT];
+	char content[MAX_CONTENT_LENGHT];
+};
+
+struct Directory {
+	char name[MAX_NAME_LENGHT];
+	struct File files[MAX_FILES_LENGHT];
+	int files_number;
+};
+
+struct MegaDirectory {
+	char name[MAX_NAME_LENGHT];
+	struct Directory directories[MAX_DIRECTORIES_LENGHT];
+};
+
+// Function Prototype
+struct File createFile(char* name, char* content);
+struct Directory createDirectory(char* name, struct File files, int files_number); 
+struct MegaDirectory createMegaDirectory(char* name, struct Directory directories);
+
+#endif
