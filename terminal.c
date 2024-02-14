d070ea5b84be2e8afbb6b3ddae9fd770fd246150
S. Issam <57065659+IssamSY@users.noreply.github.com>
2024-02-14 11:29:18 +0100

    Update terminal.c

--- terminal.c
+++ terminal.c
@@ -1,26 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-// Defining some constants
-#define MAX_NAME_LENGHT 30
-#define MAX_CONTENT_LENGHT 100
-#define MAX_FILES_LENGHT 20
-#define MAX_DIRECTORIES_LENGHT 20
-
-struct File {
-	char name[MAX_NAME_LENGHT];
-	char content[MAX_CONTENT_LENGHT];
-};
-
-struct Directory {
-	char name[MAX_NAME_LENGHT];
-	struct File files[MAX_FILES_LENGHT];
-	int files_number;
-};
-
-struct MegaDirectory {
-	char name[MAX_NAME_LENGHT];
-	struct Directory directories[MAX_DIRECTORIES_LENGHT];
-};
+#include "SysFile.h"
 
 int main() {
 	// Creating files using `struct File`
